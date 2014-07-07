11ef639b29de78a854e9ab2cd0623043d49ee018
John Pomeroy <JPCoded@gmail.com>
2014-07-06 22:57:50 -0500

    Removed expander, redid the RGBBox. changed layout some.

--- RGBBox.xaml.cs
+++ RGBBox.xaml.cs
@@ -20,9 +20,39 @@ namespace WPF_LED_Controller
     /// </summary>
     public partial class RGBBox : UserControl
     {
+        public event TextChangedEventHandler TextChanged;
         public RGBBox()
         {
             InitializeComponent();
         }
+
+        public string Text
+        {
+            get { return this.lblName.Content.ToString(); }
+            set { this.lblName.Content = value; }
+        }
+        private void txtRGB_KeyDown(object sender, KeyEventArgs e)
+        {
+            string input = e.Key.ToString().Substring(1);
+            try
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                { e.Handled = false; }
+                if (input[0] < '0' || input[0] > '9')
+                { e.Handled = false; }
+            }
+            catch { e.Handled = true; }
+        }
+
+        private void txtRGB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextChangedEventHandler h = TextChanged;
+            if (h != null)
+            {
+                h(this, e);
+            }
+        }
+
+       
     }
 }
