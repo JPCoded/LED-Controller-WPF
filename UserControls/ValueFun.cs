eec02dac71a462589122bf3f44d6c7071a53e592
JPCoded <JPCoded@gmail.com>
2017-02-06 16:29:22 -0500

    converted if statement into switch statement in valuefun

--- UserControls/ValueFun.cs
+++ UserControls/ValueFun.cs
@@ -1,58 +1,87 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Globalization;
 using System.Windows.Controls;
 using System.Windows.Input;
 
+#endregion
 
 namespace WPF_LED_Controller.UserControls
 {
-    static class ValueFun
+    internal static class ValueFun
     {
         public static string OverUnderValidation(string valueToCheck, int max = 255)
         {
             if (!string.IsNullOrEmpty(valueToCheck))
             {
-                var checkVal = Int32.Parse(valueToCheck);
+                var checkVal = int.Parse(valueToCheck);
                 if (checkVal >= max)
-                { return max.ToString(CultureInfo.InvariantCulture); }
+                {
+                    return max.ToString(CultureInfo.InvariantCulture);
+                }
                 if (checkVal < 0)
-                { return "0"; }
+                {
+                    return "0";
+                }
             }
             else
-            { return "0"; }
+            {
+                return "0";
+            }
             return valueToCheck.ToUpper();
         }
 
         public static void KeyPreview(object sender, KeyEventArgs e, int max = 255)
         {
-            if (e.Key == Key.Up)
+            switch (e.Key)
             {
-                if (string.IsNullOrEmpty(((TextBox)sender).Text))
-                { ((TextBox)sender).Text = "0"; }
-                var oldValue = Convert.ToInt32(((TextBox)sender).Text);
-                var newValue = (oldValue + 1 > max) ? max : oldValue + 1;
-                ((TextBox)sender).Text = newValue.ToString(CultureInfo.InvariantCulture);
+                case Key.Up:
+                {
+                    if (string.IsNullOrEmpty(((TextBox) sender).Text))
+                    {
+                        ((TextBox) sender).Text = "0";
+                    }
+                    var oldValue = Convert.ToInt32(((TextBox) sender).Text);
+                    var newValue = (oldValue + 1 > max) ? max : oldValue + 1;
+                    ((TextBox) sender).Text = newValue.ToString();
+                }
+                    break;
+                case Key.Down:
+                {
+                    if (string.IsNullOrEmpty(((TextBox) sender).Text))
+                    {
+                        ((TextBox) sender).Text = max.ToString();
+                    }
+                    var oldValue = Convert.ToInt32(((TextBox) sender).Text);
+                    var newValue = (oldValue - 1 < 0) ? 0 : oldValue - 1;
+                    ((TextBox) sender).Text = newValue.ToString();
+                }
+                    break;
+                default:
+                    if (e.Key == Key.Back || e.Key == Key.Left || e.Key == Key.Right ||
+                        (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || (e.Key >= Key.D0 && e.Key <= Key.D9))
+                    {
+                        e.Handled = false;
+                    }
+                    else
+                    {
+                        e.Handled = true;
+                    }
+                    break;
             }
-            else if (e.Key == Key.Down)
-            {
-                if (string.IsNullOrEmpty(((TextBox)sender).Text))
-                { ((TextBox)sender).Text = max.ToString(CultureInfo.InvariantCulture); }
-                var oldValue = Convert.ToInt32(((TextBox)sender).Text);
-                var newValue = (oldValue - 1 < 0) ? 0 : oldValue - 1;
-                ((TextBox)sender).Text = newValue.ToString(CultureInfo.InvariantCulture);
-            }
-            else if (e.Key == Key.Back || e.Key == Key.Left || e.Key == Key.Right || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) || (e.Key >= Key.D0 && e.Key <= Key.D9))
-            { e.Handled = false; }
-            else
-            { e.Handled = true; }
         }
 
         public static void HexKeyValidation(KeyEventArgs e)
         {
             var input = e.Key.ToString();
             if (e.Key == Key.D3 && (e.Key == Key.LeftShift || e.Key == Key.RightShift))
-            { input = "#"; }
-            if (!(input == "#" || (input[0] >= 'A' && input[0] <= 'F') || (input[0] >= 'a' && input[0] <= 'F') || (input[0] >= '0' && input[0] <= '9')))
+            {
+                input = "#";
+            }
+            if (
+                !(input == "#" || (input[0] >= 'A' && input[0] <= 'F') || (input[0] >= 'a' && input[0] <= 'F') ||
+                  (input[0] >= '0' && input[0] <= '9')))
             {
                 e.Handled = true;
             }
