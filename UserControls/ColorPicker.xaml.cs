d5035ab4dc920117b7055dad943a40a9f3e3203d
JPCoded <JPCoded@gmail.com>
2017-02-06 16:47:54 -0500

    more code cleanup and text corrections

--- UserControls/ColorPicker.xaml.cs
+++ UserControls/ColorPicker.xaml.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Globalization;
+﻿#region
+
+using System;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
+#endregion
+
 namespace WPF_LED_Controller.UserControls
 {
     /// <summary>
-    /// Interaction logic for ColorPicker.xaml
-    /// 
+    ///     Interaction logic for ColorPicker.xaml
     /// </summary>
     public partial class ColorPicker : UserControl
     {
@@ -17,74 +19,72 @@ namespace WPF_LED_Controller.UserControls
             InitializeComponent();
         }
 
-        #region TextBoxes
-
-        #region TextChanged
-        private void txtRed_TextChanged(object sender, TextChangedEventArgs e)
+  private void txtRed_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ((TextBox)sender).Text = ValueFun.OverUnderValidation(((TextBox)sender).Text);
-            
+            ((TextBox) sender).Text = ValueFun.OverUnderValidation(((TextBox) sender).Text);
+
             if (((TextBox) sender).Text == canColor.Red.ToString()) return;
-            //Convert textbox to byte, but check to see if it's empty, if so send 0
-            var rbyteValue = Convert.ToByte(((TextBox)sender).Text);
-            //change red vaule of main color
+            //Convert text box to byte, but check to see if it's empty, if so send 0
+            var rbyteValue = Convert.ToByte(((TextBox) sender).Text);
+            //change red value of main color
             canColor.SavedColor = Color.FromRgb(rbyteValue, canColor.Green, canColor.Blue);
         }
 
         private void txtGreen_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ((TextBox)sender).Text = ValueFun.OverUnderValidation(((TextBox)sender).Text);
+            ((TextBox) sender).Text = ValueFun.OverUnderValidation(((TextBox) sender).Text);
 
             if (((TextBox) sender).Text == canColor.Green.ToString()) return;
-            //Convert textbox to byte, but check to see if it's empty, if so send 0
-            var gbyteValue = Convert.ToByte(((TextBox)sender).Text);
+            //Convert text box to byte, but check to see if it's empty, if so send 0
+            var gbyteValue = Convert.ToByte(((TextBox) sender).Text);
             //change green value of main color
             canColor.SavedColor = Color.FromRgb(canColor.Red, gbyteValue, canColor.Blue);
         }
 
         private void txtBlue_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ((TextBox)sender).Text = ValueFun.OverUnderValidation(((TextBox)sender).Text);
+            ((TextBox) sender).Text = ValueFun.OverUnderValidation(((TextBox) sender).Text);
 
             if (((TextBox) sender).Text == canColor.Blue.ToString()) return;
-            //Convert textbox to byte, but check to see if it's empty, if so send 0
-            var bbyteValue = Convert.ToByte(((TextBox)sender).Text);
-            //change blue vaule of main color
+            //Convert text box to byte, but check to see if it's empty, if so send 0
+            var bbyteValue = Convert.ToByte(((TextBox) sender).Text);
+            //change blue value of main color
             canColor.SavedColor = Color.FromRgb(canColor.Red, canColor.Green, bbyteValue);
         }
 
-        #endregion
+   
 
-        #region PreviewKeyDown
+   
 
         private void txtRGB_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            ValueFun.KeyPreview(sender,e);
+            ValueFun.KeyPreview(sender, e);
         }
 
-        #endregion
 
-        #region KeyDown
+
+       
 
         /// <summary>
-        /// KeyDown for the AllHex textbox
+        ///     KeyDown for the AllHex textbox
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void txtHAll_KeyDown(object sender, KeyEventArgs e)
         {
             ValueFun.HexKeyValidation(e);
-          
-                var strHex = ((TextBox) sender).Text;
-                //check to see if it's full hex with either 6 digits (no alpha) or 8 digits (with alpha) plus #, if they arent, we go no farther in code.
+
+            var strHex = ((TextBox) sender).Text;
+            //check to see if it's full hex with either 6 digits (no alpha) or 8 digits (with alpha) plus #, if they arent, we go no farther in code.
             if ((strHex.Length != 7 && strHex.Length != 9) || strHex[0] != '#') return;
-           
+
             var convertFromString = ColorConverter.ConvertFromString(strHex);
             if (convertFromString != null)
                 canColor.SavedColor = (Color) convertFromString;
         }
-        #endregion
 
-        #endregion     
+   
+
+       
     }
 }
--- UserControls/Disco.xaml.cs
+++ UserControls/Disco.xaml.cs
@@ -1,54 +1,47 @@
-﻿using System;
-using System.Globalization;
+﻿#region
+
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
+#endregion
+
 namespace WPF_LED_Controller.UserControls
 {
     /// <summary>
-    /// Interaction logic for Disco.xaml
+    ///     Interaction logic for Disco.xaml
     /// </summary>
     public partial class Disco : Window
     {
-      
         public Disco()
         {
-           
             InitializeComponent();
-           txtGMax.Text = "255";
+            txtGMax.Text = "255";
             txtRMax.Text = "255";
             txtBMax.Text = "255";
 
             btnSet.Click += (sender, e) => Visibility = Visibility.Hidden;
-
         }
 
-
         public byte RedMin => Convert.ToByte(txtRMin.Text);
-
         public byte RedMax => Convert.ToByte(txtRMax.Text);
-
         public byte GreenMin => Convert.ToByte(txtGMin.Text);
-
         public byte GreenMax => Convert.ToByte(txtGMax.Text);
-
         public byte BlueMin => Convert.ToByte(txtBMin.Text);
-
         public byte BlueMax => Convert.ToByte(txtBMax.Text);
-
 
         private void txtMinMax_OnPreviewKeyDown(object sender, KeyEventArgs e)
         {
-            ValueFun.KeyPreview(sender,e);
+            ValueFun.KeyPreview(sender, e);
         }
 
         private void txtGMin_TextChanged(object sender, TextChangedEventArgs e)
         {
-            txtGMin.Text = ValueFun.OverUnderValidation(txtGMin.Text,254);
+            txtGMin.Text = ValueFun.OverUnderValidation(txtGMin.Text, 254);
             txtGMax.Text = ValueFun.OverUnderValidation(txtGMax.Text);
             var min = Convert.ToInt32(txtGMin.Text);
-           
+
             var max = Convert.ToInt32(txtGMax.Text);
             if (min > max)
             {
@@ -58,20 +51,20 @@ namespace WPF_LED_Controller.UserControls
 
         private void txtGMax_TextChanged(object sender, TextChangedEventArgs e)
         {
-           txtGMax.Text = ValueFun.OverUnderValidation(txtGMax.Text);
-           txtGMin.Text = ValueFun.OverUnderValidation(txtGMin.Text,254);
-            
+            txtGMax.Text = ValueFun.OverUnderValidation(txtGMax.Text);
+            txtGMin.Text = ValueFun.OverUnderValidation(txtGMin.Text, 254);
+
             var min = Convert.ToInt32(txtGMin.Text);
             var max = Convert.ToInt32(txtGMax.Text);
             if (min > max)
             {
-               txtGMax.Text = (min + 1).ToString();
+                txtGMax.Text = (min + 1).ToString();
             }
         }
 
         private void txtBMin_TextChanged(object sender, TextChangedEventArgs e)
         {
-            txtBMin.Text = ValueFun.OverUnderValidation(txtBMin.Text,254);
+            txtBMin.Text = ValueFun.OverUnderValidation(txtBMin.Text, 254);
             txtBMax.Text = ValueFun.OverUnderValidation(txtBMax.Text);
             var min = Convert.ToInt32(txtBMin.Text);
             var max = Convert.ToInt32(txtBMax.Text);
@@ -84,7 +77,7 @@ namespace WPF_LED_Controller.UserControls
         private void txtBMax_TextChanged(object sender, TextChangedEventArgs e)
         {
             txtBMax.Text = ValueFun.OverUnderValidation(txtBMax.Text);
-            txtBMin.Text = ValueFun.OverUnderValidation(txtBMin.Text,254);
+            txtBMin.Text = ValueFun.OverUnderValidation(txtBMin.Text, 254);
             var min = Convert.ToInt32(txtBMin.Text);
             var max = Convert.ToInt32(txtBMax.Text);
             if (min > max)
@@ -95,8 +88,7 @@ namespace WPF_LED_Controller.UserControls
 
         private void txtRMin_TextChanged(object sender, TextChangedEventArgs e)
         {
-
-            txtRMin.Text = ValueFun.OverUnderValidation(txtRMin.Text,254);
+            txtRMin.Text = ValueFun.OverUnderValidation(txtRMin.Text, 254);
             txtRMax.Text = ValueFun.OverUnderValidation(txtRMax.Text);
             var min = Convert.ToInt32(txtRMin.Text);
             var max = Convert.ToInt32(txtRMax.Text);
@@ -108,7 +100,7 @@ namespace WPF_LED_Controller.UserControls
 
         private void txtRMax_TextChanged(object sender, TextChangedEventArgs e)
         {
-            txtRMin.Text = ValueFun.OverUnderValidation(txtRMin.Text,254);
+            txtRMin.Text = ValueFun.OverUnderValidation(txtRMin.Text, 254);
             txtRMax.Text = ValueFun.OverUnderValidation(txtRMax.Text);
             var min = Convert.ToInt32(txtRMin.Text);
             var max = Convert.ToInt32(txtRMax.Text);
